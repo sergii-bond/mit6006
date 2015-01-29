9437ee4bfec2949cc01c66b3cee74fb69cfacf88
sergii-bondariev <SergeyBondarev@gmail.com>
2015-01-29 07:56:10 -0800

    sweep line implementation

--- ps3/circuit2/circuit2.py
+++ ps3/circuit2/circuit2.py
@@ -326,10 +326,12 @@ class CrossVerifier(object):
 
   def _events_from_layer(self, layer):
     """Populates the sweep line events from the wire layer."""
-    left_edge = min([wire.x1 for wire in layer.wires.values()])
+    #left_edge = min([wire.x1 for wire in layer.wires.values()])
     for wire in layer.wires.values():
       if wire.is_horizontal():
-        self.events.append([left_edge, 0, wire.object_id, 'add', wire])
+        #self.events.append([left_edge, 0, wire.object_id, 'add', wire])
+        self.events.append([wire.x1, 0, wire.object_id, 'add', wire])
+        self.events.append([wire.x2, 2, wire.object_id, 'remove', wire])
       else:
         self.events.append([wire.x1, 1, wire.object_id, 'query', wire])
 
@@ -343,20 +345,22 @@ class CrossVerifier(object):
     for event in self.events:
       event_x, event_type, wire = event[0], event[3], event[4]
       
-      if event_type == 'add':
+      self.trace_sweep_line(event_x)
+
+      if event_type == 'remove':
+        self.index.remove(KeyWirePair(wire.y1, wire))
+        #pass
+      elif event_type == 'add':
         self.index.add(KeyWirePair(wire.y1, wire))
       elif event_type == 'query':
-        self.trace_sweep_line(event_x)
-        cross_wires = []
-        for kwp in self.index.list(KeyWirePairL(wire.y1),
-                                   KeyWirePairH(wire.y2)):
-          if wire.intersects(kwp.wire):
-            cross_wires.append(kwp.wire)
         if count_only:
-          result += len(cross_wires)
+            result += self.index.count(KeyWirePairL(wire.y1),
+                                   KeyWirePairH(wire.y2))
         else:
-          for cross_wire in cross_wires:
-            result.add_crossing(wire, cross_wire)
+            cross_wires = []
+            for kwp in self.index.list(KeyWirePairL(wire.y1),
+                                    KeyWirePairH(wire.y2)):
+                result.add_crossing(wire, kwp.wire)
 
     return result
   
